8e457fcef0677c0faee886b263cc8734380f5c08
Gumblehod <150657019+Gumblehod@users.noreply.github.com>
2023-11-13 16:33:02 +0800

    Add files via upload

---
+++ Life++/LifePP/src/test/java/com/anq/LifePP/LifePpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anq.LifePP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifePpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
