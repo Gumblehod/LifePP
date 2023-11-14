537256e21dea53021d939020745da5c7b77b3db3
daestortion <danielgilbertd@gmail.com>
2023-11-14 17:09:38 +0800

    origin

---
+++ Life++/LifePP/src/main/java/com/anq/LifePP/Entity/UserEntity.java
@@ -0,0 +1,103 @@
+package com.anq.LifePP.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+
+@Entity
+@Table(name="tblUser")
+public class UserEntity {
+	
+	@Id
+	@Column(name="username")
+	private String username;
+	@Column(name="fname")
+	private String fname;
+	@Column(name="lname")
+	private String lname;
+	@Column(name="email")
+	private String email;
+	@Column(name="pnum")
+	private int pnum;
+	@Column(name="gender")
+	private String gender;
+	@Column(name="birthdate")
+	private String birthdate;
+	@Column(name="password")
+	private String password;
+	@Column(name="type")
+	private int type;
+	
+	public UserEntity() {}
+	
+	public UserEntity(String username, String fname, String lname, String email, int pnum, String gender,
+			String birthdate, String password, int type) {
+		super();
+		this.username = username;
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.pnum = pnum;
+		this.gender = gender;
+		this.birthdate = birthdate;
+		this.password = password;
+		this.type = type;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPnum() {
+		return pnum;
+	}
+	public void setPnum(int pnum) {
+		this.pnum = pnum;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+}
