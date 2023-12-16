900090ad5dd7945893cf05ec019e7fe2427e51a8
daestortion <danielgilbertd@gmail.com>
2023-12-17 03:44:47 +0800

    origin

--- Life++/LifePP/src/main/java/com/anq/LifePP/Service/UserService.java
+++ Life++/LifePP/src/main/java/com/anq/LifePP/Service/UserService.java
@@ -5,8 +5,7 @@ import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 import com.anq.LifePP.Entity.AchievementEntity;
 import com.anq.LifePP.Entity.CourseEntity;
